71f840bbaad7945fc296f8e34d15433ed9d5ea60
ananddeq <aparadkar@deqode.com>
2023-04-13 16:45:02 +0530

    script added

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-def hello():
-    print("hi")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+    return "Welcome to TAJ residency"
 
 
-def bye():
-    print("bye")
+@app.get("/api")
+def get_room():
+    return "Inside the room"
 
 
-print(hello())
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port="5000")
 
-bye()
